47e26ac288550110027c53b602808f966039f723
GabNameTaken <88995312+GabNameTaken@users.noreply.github.com>
2023-12-09 03:46:00 +0800

    cleanse particles

--- Assets/Scripts/Entities/Cecilia/Blessing.cs
+++ Assets/Scripts/Entities/Cecilia/Blessing.cs
@@ -17,4 +17,18 @@ public class Blessing : Skill
         
         return -damage;
     }
+
+    protected override IEnumerator SkillAnimationCoroutine(EntityBase attacker, List<EntityBase> attackeeList)
+    {
+        yield return null;
+
+        base.SkillAnimationCoroutine(attacker, attackeeList);
+
+        yield return new WaitForSeconds(attacker.animator.GetCurrentAnimatorStateInfo(0).length * 0.3f);
+
+        SkillParticle particle = Instantiate(skillParticle, attacker.transform);
+        particle.Play();
+
+        yield return base.SkillAnimationCoroutine(attacker, attackeeList);
+    }
 }
