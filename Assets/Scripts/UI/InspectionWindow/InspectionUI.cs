8ae0c75ebed01224c32426261f2395b7453684ee
GabNameTaken <88995312+GabNameTaken@users.noreply.github.com>
2024-01-15 18:21:04 +0800

    inspection window in progress

---
+++ Assets/Scripts/UI/InspectionWindow/InspectionUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InspectionUI : MonoBehaviour
+{
+    public EntityBase inspectEntity;
+
+    [SerializeField] TMP_Text nameText;
+    [SerializeField] GameObject tabsGO;
+
+}
